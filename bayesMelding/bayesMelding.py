1579a805763abc5d3aa915d252b40285bd9c45df
Xiaoyu Xiong <xx249@Xiaoyus-MacBook-Pro.local>
2018-02-08 12:32:34 +0000

    add the multiplicative bias parameter b with normal prior (mean=0, sd=1)

--- bayesMelding/bayesMelding.py
+++ bayesMelding/bayesMelding.py
@@ -67,6 +67,12 @@ def read_Sim_Data():
 
     return[X_hatZs, y_hatZs, X_tildZs, y_tildZs]
 
+def log_like_normal(w, mu, Sigma):
+    l_chol = np.linalg.cholesky(Sigma)
+    u = linalg.solve_triangular(l_chol.T, linalg.solve_triangular(l_chol, w, lower=True))
+    log_like_normal = -np.sum(np.log(np.diag(l_chol))) - 0.5 * np.dot(w, u) - 0.5 * len(w) * np.log(2*np.pi)
+    return log_like_normal
+
 def log_like_gamma(w, shape, rate):
     res = np.sum(shape * w - rate * np.exp(w) + shape * np.log(rate) - np.log(gamma(shape)))
     return res
@@ -80,11 +86,11 @@ def log_obsZs_giv_par(theta, X_hatZs, y_hatZs, X_tildZs, y_tildZs, OMEGA = 1e-6)
     # log_sigma = theta[0]
     # log_w = theta[1:]
 
-    #adding parameters of sigma, obs_noi_scale
+    #adding parameters of sigma, obs_noi_scale, mulplicative bias b
     log_sigma = theta[0]
-    log_w = theta[1:-1]
-    log_obs_noi_scale = theta[-1]
-    # b = theta[-1]
+    log_w = theta[1:-2]
+    log_obs_noi_scale = theta[-2]
+    b = theta[len(theta) -1:]
 
     n_hatZs = X_hatZs.shape[0]
     n_tildZs = X_tildZs.shape[0]  
@@ -93,12 +99,12 @@ def log_obsZs_giv_par(theta, X_hatZs, y_hatZs, X_tildZs, y_tildZs, OMEGA = 1e-6)
     mat = np.zeros(n_bothZs * n_bothZs).reshape(n_bothZs, n_bothZs)
 
     C_hatZs = gpGaussLikeFuns.cov_matrix_reg(X = X_hatZs, sigma = np.exp(log_sigma), w = np.exp(log_w), obs_noi_scale = np.exp(log_obs_noi_scale))
-    C_tildZs = cov_areal(areal_coordinate = X_tildZs, sigma = np.exp(log_sigma), w = np.exp(log_w))
+    C_tildZs = cov_areal(areal_coordinate = X_tildZs, sigma = np.exp(log_sigma), w = np.exp(log_w), b=b)
 
     mat[:n_hatZs, :n_hatZs] = C_hatZs
     mat[n_hatZs:n_hatZs + n_tildZs, n_hatZs:n_hatZs + n_tildZs] = C_tildZs
     
-    point_areal = np.array([avg_cov_point_areal(X_hatZs, X_tildZs[i], sigma = np.exp(log_sigma), w = np.exp(log_w)) for i in range(n_tildZs)])
+    point_areal = np.array([avg_cov_point_areal(X_hatZs, X_tildZs[i], sigma = np.exp(log_sigma), w = np.exp(log_w), b=b) for i in range(n_tildZs)])
     mat[n_hatZs:n_hatZs + n_tildZs, :n_hatZs] = point_areal
     mat[:n_hatZs, n_hatZs:n_hatZs + n_tildZs] = point_areal.T
 
@@ -116,7 +122,10 @@ def log_obsZs_giv_par(theta, X_hatZs, y_hatZs, X_tildZs, y_tildZs, OMEGA = 1e-6)
     len_scal_rate = 1./np.sqrt(num_len_scal)
     obs_noi_scale_shape = 1.2
     obs_noi_scale_rate = 0.6
-    log_prior = log_like_gamma(log_sigma, sigma_rate, sigma_shape) + log_like_gamma(log_w, len_scal_shape, len_scal_rate) +  log_like_gamma(log_obs_noi_scale, obs_noi_scale_shape, obs_noi_scale_rate)
+    b_mu =0.
+    b_Sigma = np.diag([1.])
+    #sigma, length_sacle, obs_noi_scale have to take positive numbers, thus taking gamma priors, whereas the mutiplicative bias b takes a normal prior
+    log_prior = log_like_gamma(log_sigma, sigma_rate, sigma_shape) + log_like_gamma(log_w, len_scal_shape, len_scal_rate) + log_like_gamma(log_obs_noi_scale, obs_noi_scale_shape, obs_noi_scale_rate) + log_like_normal(b, b_mu, b_Sigma)
 
     #compute the logarithm of the posterior
     log_pos = joint_log_like + log_prior
@@ -132,11 +141,11 @@ def minus_log_obsZs_giv_par(theta, X_hatZs, y_hatZs, X_tildZs, y_tildZs, OMEGA =
     # log_sigma = theta[0]
     # log_w = theta[1:]
 
-    #adding parameters of sigma, obs_noi_scale
+    #adding parameters of sigma, obs_noi_scale, mulplicative bias b
     log_sigma = theta[0]
-    log_w = theta[1:-1]
-    log_obs_noi_scale = theta[-1]
-    # b = theta[-1]
+    log_w = theta[1:-2]
+    log_obs_noi_scale = theta[-2]
+    b = theta[len(theta)-1:]
 
     n_hatZs = X_hatZs.shape[0]
     n_tildZs = X_tildZs.shape[0]  
@@ -145,12 +154,12 @@ def minus_log_obsZs_giv_par(theta, X_hatZs, y_hatZs, X_tildZs, y_tildZs, OMEGA =
     mat = np.zeros(n_bothZs * n_bothZs).reshape(n_bothZs, n_bothZs)
 
     C_hatZs = gpGaussLikeFuns.cov_matrix_reg(X = X_hatZs, sigma = np.exp(log_sigma), w = np.exp(log_w), obs_noi_scale = np.exp(log_obs_noi_scale))
-    C_tildZs = cov_areal(areal_coordinate = X_tildZs, sigma = np.exp(log_sigma), w = np.exp(log_w))
+    C_tildZs = cov_areal(areal_coordinate = X_tildZs, sigma = np.exp(log_sigma), w = np.exp(log_w), b=b)
 
     mat[:n_hatZs, :n_hatZs] = C_hatZs
     mat[n_hatZs:n_hatZs + n_tildZs, n_hatZs:n_hatZs + n_tildZs] = C_tildZs
     
-    point_areal = np.array([avg_cov_point_areal(X_hatZs, X_tildZs[i], sigma = np.exp(log_sigma), w = np.exp(log_w)) for i in range(n_tildZs)])
+    point_areal = np.array([avg_cov_point_areal(X_hatZs, X_tildZs[i], sigma = np.exp(log_sigma), w = np.exp(log_w), b=b) for i in range(n_tildZs)])
 
     mat[n_hatZs:n_hatZs + n_tildZs, :n_hatZs] = point_areal
     mat[:n_hatZs, n_hatZs:n_hatZs + n_tildZs] = point_areal.T
@@ -169,8 +178,10 @@ def minus_log_obsZs_giv_par(theta, X_hatZs, y_hatZs, X_tildZs, y_tildZs, OMEGA =
     len_scal_rate = 1./np.sqrt(num_len_scal)
     obs_noi_scale_shape = 1.2
     obs_noi_scale_rate = 0.6
-    log_prior = log_like_gamma(log_sigma, sigma_rate, sigma_shape) + log_like_gamma(log_w, len_scal_shape, len_scal_rate) +  log_like_gamma(log_obs_noi_scale, obs_noi_scale_shape, obs_noi_scale_rate)
-
+    b_mu =0.
+    b_Sigma = np.diag([1.])
+    #sigma, length_sacle, obs_noi_scale have to take positive numbers, thus taking gamma priors, whereas the mutiplicative bias b takes a normal prior
+    log_prior = log_like_gamma(log_sigma, sigma_rate, sigma_shape) + log_like_gamma(log_w, len_scal_shape, len_scal_rate) + log_like_gamma(log_obs_noi_scale, obs_noi_scale_shape, obs_noi_scale_rate) + log_like_normal(b, b_mu, b_Sigma)
     #compute the logarithm of the posterior
     log_pos = joint_log_like + log_prior
 
@@ -183,12 +194,13 @@ def optimize(X_hatZs, y_hatZs, X_tildZs, y_tildZs, repeat=1, method='BFGS', rbf=
         num_par=1
     else:
         num_par=X_hatZs.shape[1]   
-
+    print 'starting optimising the parameters'
     res = []
     count = 0
     while count != repeat:
         try:
-            initial_theta=np.concatenate((np.log(np.random.gamma(1.2, 5., 1)), np.log(np.random.gamma(1., np.sqrt(num_par), num_par)), np.log(np.random.gamma(1.2, 1/0.6, 1))), axis=0)
+            initial_theta=np.concatenate((np.log(np.random.gamma(1.2, 5., 1)), np.log(np.random.gamma(1., np.sqrt(num_par), num_par)), np.log(np.random.gamma(1.2, 1./0.6, 1)), np.random.normal(0., 1., 1)), axis=0)
+            print 'initial theta is ' + str(initial_theta)
             tmp_res = minimize(fun=minus_log_obsZs_giv_par, 
                            x0=initial_theta, method=method,
                            jac=False,
@@ -208,9 +220,9 @@ def optimize(X_hatZs, y_hatZs, X_tildZs, y_tildZs, repeat=1, method='BFGS', rbf=
     res = res.T
     print 'minus_log_like for repeat ' + str(repeat) + ' is ' + str(res[1, :])
     i = np.argmin(res[1,:])
-    print 'log_parameters after optimisation is :'  + str(np.array(res[0, :][i]))
-    print 'parameters after optimisation is :'  + str(np.exp(np.array(res[0, :][i])))
-    print 'covariance of log_pars after optimisation is :'  + str(np.array(res[2, :][i]))
+    print 'log_parameters plus mulplicative_bias after optimisation is :'  + str(np.array(res[0, :][i]))
+    print 'parameters after optimisation is :'  + str([np.exp(np.array(res[0, :][i])[:-1]), np.array(res[0, :][i])[-1]])
+    print 'covariance of pars after optimisation is :'  + str(np.array(res[2, :][i]))
     return [np.array(res[0, :][i]), np.array(res[2, :][i])]
 
 class MH_estimator():
@@ -243,16 +255,17 @@ class MH_estimator():
             accept_rate = (float)(accept_count) / (float)(nbatch)
             print 'Acc_Rate for the ' + str(i) + ' iteration is ' + str(accept_rate)
 
-            if accept_rate >= 0.20 and accept_rate <= 0.38:
+            if accept_rate >= 0.20 and accept_rate <= 0.36:
                 iter_index = i
                 break
             if accept_rate < 0.20:
                 cov *= 0.8
-            if accept_rate > 0.38:
+            if accept_rate > 0.36:
                 cov /= 0.8
         return iter_index, cov, tmp_mu
 
     def sampler(self, cov, mu, size=1000, OMEGA = 1e-6):
+        print 'starting sampling'
         sample = np.zeros([len(mu), size])
         acc_count = 0
         l_cov = np.linalg.cholesky(cov)
@@ -281,10 +294,10 @@ class MH_estimator():
     def estimate(self, size = 100, proposal = 'identity', rbf = True):
         while True:
             try:
-                mu, hess_inv= optimize(self.X_hatZs, self.y_hatZs, self.X_tildZs, self.y_tildZs, repeat=1, method='BFGS', rbf=True, OMEGA = 1e-6)
+                mu, hess_inv= optimize(self.X_hatZs, self.y_hatZs, self.X_tildZs, self.y_tildZs, repeat=5, method='BFGS', rbf=True, OMEGA = 1e-6)
                 print 'diag of the hess_inv is ' + str(np.diag(hess_inv))
-                while np.diag(hess_inv)[0] < 1e-4:
-                    mu, hess_inv= optimize(self.X_hatZs, self.y_hatZs, self.X_tildZs, self.y_tildZs, repeat=1, method='BFGS', rbf=True, OMEGA = 1e-6)
+                # while np.diag(hess_inv)[0] < 1e-4:
+                #     mu, hess_inv= optimize(self.X_hatZs, self.y_hatZs, self.X_tildZs, self.y_tildZs, repeat=1, method='BFGS', rbf=True, OMEGA = 1e-6)
                 break
             except:
                 continue
@@ -302,14 +315,14 @@ class MH_estimator():
             iter, cov, mu = self.burningin(mu, cov, nburnin=nburnin)
             nburnin += 2000
 
-        print 'log_parameters after burningin is :'  + str(mu)
-        print 'parameters after burningin is :'  + str(np.exp(mu))
-        print 'cov of log_pars after burningin is :'  + str(cov)
+        print 'log_parameters  plus mulplicative_bias after burningin is :'  + str(mu)
+        print 'parameters after burningin is :'  + str([np.exp(mu[:-1]), mu[-1]])
+        print 'cov of pars after burningin is :'  + str(cov)
 
         # cov = hess_inv
 
         sample = self.sampler(cov, mu, size = size)
-        sample = np.exp(sample)
+        sample[:-1, :] = np.exp(sample[:-1, :])
 
         sample_mod = stats.mode(sample, axis =1)
         sample_mean = np.mean(sample, axis=1)
@@ -323,7 +336,7 @@ if __name__ == '__main__':
     X_hatZs, y_hatZs, X_tildZs, y_tildZs = read_Sim_Data()
     start = default_timer()
     tmp = MH_estimator(X_hatZs, y_hatZs, X_tildZs, y_tildZs)
-    size = 800
+    size = 1000
     proposal ='fullApproxCov'
     mod, mu, std = tmp.estimate(size=size, proposal = proposal)
     end = default_timer()
@@ -331,6 +344,13 @@ if __name__ == '__main__':
     print 'estimated mod of parameters is ' + str(mod)
     print 'estimated mean of parameters is ' + str(mu)
     print 'estimated std of parameters is ' + str(std)
+
+    # theta = np.array([0., 0., np.log(.1), 1.])
+    # log_obsZs_giv_par(theta, X_hatZs, y_hatZs, X_tildZs, y_tildZs)
+
+
+
+
 
 
  
