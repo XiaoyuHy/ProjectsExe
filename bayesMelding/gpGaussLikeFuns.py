547d215862a3e8be5aab08ea397c16fa4275364c
Xiaoyu Xiong <xx249@Xiaoyus-MacBook-Pro.local>
2018-07-06 12:55:35 +0100

    add fun of prediction for gp regression; add variance of observation noise to variance of the gp prediction

--- bayesMelding/gpGaussLikeFuns.py
+++ bayesMelding/gpGaussLikeFuns.py
@@ -2,6 +2,9 @@ import numpy as np
 from scipy import linalg
 import computeN3Cost
 import numbers
+import pickle
+import argparse
+from itertools import chain
 
 np.seterr(over='raise', divide='raise')
 
@@ -35,6 +38,133 @@ def cov_matrix_reg(X, w, sigma=1., obs_noi_scale=0.1, OMEGA = 1e-6):
     C = K + np.diag(np.repeat(obs_noi_scale**2, n))
     return C
 
+def cov_mat_xy(x, y, sigma, w):
+    w = np.array(w)
+    if len(w) == 1:
+        w = np.repeat(w, x.shape[1])
+    n_x = x.shape[0]
+    n_y = y.shape[0]
+    d = x.shape[1]
+    w[w < 1e-19] = 1e-19
+    W = np.eye(d)
+    W[np.diag_indices(d)] = 1./w**2
+
+    tmp1 = np.sum(np.dot(x, W) * x, axis=1)
+    tmp2 = np.sum(np.dot(y, W) * y, axis=1)
+    tmp3 = np.dot(x, np.dot(W, y.T))
+    
+    square_dist = tmp1.reshape(n_x, 1) + tmp2.reshape(1, n_y) - 2 * tmp3
+
+    cov_of_two_vec = sigma * np.exp(- 0.5 * square_dist) # is a matrix of size (n_train, n_y)
+    return cov_of_two_vec
+
+# funtion that compute the point_areal covariance and its gradients with respect to [log_sigmaZs, log_phiZs, b ](order in the list)
+def point_areal(X_hatZs, areal_coordinate, log_sigma_Zs, log_phi_Zs, b, pointArealFlag = True):
+    num_areas = len(areal_coordinate)
+    grad_C_pointAreal_upper = []
+    grad_C_pointAreal_lower = []
+
+    tmp0 = [deri_avg_grad_C_point_and_arealZs_No_obs_noi(X_hatZs, areal_coordinate[j], log_sigma_Zs, log_phi_Zs, b, pointArealFlag) \
+    for j in range(num_areas)]
+    avg_pointAreal_lower = np.array([tmp0[i][0] for i in range(num_areas)])
+    avg_pointAreal_upper = avg_pointAreal_lower.T 
+    deriSigmaZsphiZsBbias = [tmp0[i][1:] for i in range(num_areas)]
+    for i in range(len(deriSigmaZsphiZsBbias[0])):
+        tmp1 = np.array([deriSigmaZsphiZsBbias[j][i] for j in range(len(deriSigmaZsphiZsBbias))])
+        grad_C_pointAreal_lower.append(tmp1)
+        grad_C_pointAreal_upper.append(tmp1.T)
+
+    return [avg_pointAreal_lower, avg_pointAreal_upper, grad_C_pointAreal_lower, grad_C_pointAreal_upper]
+
+def cov_areal(areal_coordinate, log_sigma_Zs, log_phi_Zs, b, log_sigma_deltas_of_modelOut, gp_deltas_modelOut, log_phi_deltas_of_modelOut = None,  \
+     areal_res=20, OMEGA = 1e-6):
+    num_areas = len(areal_coordinate)
+    grad_C_tildZs = []
+
+    if gp_deltas_modelOut:
+        cov_areas_tildZs = []
+        cov_areas_deltas_Mo = []
+        deri_areal_pars = []
+        for i in range(num_areas):
+            tmp0 = [deri_avg_grad_C_point_and_arealZs_No_obs_noi(areal_coordinate[i], areal_coordinate[j], log_sigma_Zs, log_phi_Zs, b) for j in range(num_areas)]
+            avgCovZs = [tmp0[ii][0] for ii in range(num_areas)]
+            deriSigmaZsphiZsBbias = [tmp0[ii][1:] for ii in range(num_areas)]
+            cov_areas_tildZs.append(avgCovZs)
+
+            tmp1 = [deri_avg_grad_C_No_obs_noi(areal_coordinate[i], areal_coordinate[j], log_sigma_deltas_of_modelOut, log_phi_deltas_of_modelOut) for j in range(num_areas)]
+            
+            avgCovDeltas = [tmp1[iii][0] for iii in range(num_areas)]
+            deriSigDtsphiDts = [tmp1[iii][1:] for iii in range(num_areas)]
+            cov_areas_deltas_Mo.append(avgCovDeltas)
+
+            tmp2 = [deriSigDtsphiDts[j] + deriSigmaZsphiZsBbias[j] for j in range(num_areas)]
+            assert len(tmp2) == num_areas
+            deri_areal_pars.append(tmp2)
+
+        tmp3 = list(chain.from_iterable(deri_areal_pars))
+        for ii in range(len(tmp3[0])):
+            tmp = np.array([tmp3[jj][ii] for jj in range(len(tmp3))]).reshape(num_areas, num_areas)
+            grad_C_tildZs.append(tmp)
+
+        covAreas = np.hstack(cov_areas_tildZs).reshape(num_areas, num_areas) + np.hstack(cov_areas_deltas_Mo).reshape(num_areas, num_areas) +\
+        np.diag(np.repeat(OMEGA, num_areas))
+        return [covAreas, grad_C_tildZs]
+    else:
+        cov_areas_tildZs = []
+        deri_areal_pars = []
+        grad_C_tildZs.append(np.diag(np.repeat(np.exp(log_sigma_deltas_of_modelOut), num_areas)))
+        for i in range(num_areas):
+
+            tmp1 = [deri_avg_grad_C_point_and_arealZs_No_obs_noi(areal_coordinate[i], areal_coordinate[j], log_sigma_Zs, log_phi_Zs, b) \
+            for j in range(num_areas)]
+            avgCovZs = [tmp1[ii][0] for ii in range(num_areas)]
+            deriSigmaZsphiZsBbias = [tmp1[ii][1:] for ii in range(num_areas)]
+            cov_areas_tildZs.append(avgCovZs)
+            deri_areal_pars.append(deriSigmaZsphiZsBbias)
+
+        tmp2 = list(chain.from_iterable(deri_areal_pars))
+        for ii in range(len(tmp2[0])):
+            tmp = np.array([tmp2[jj][ii] for jj in range(len(tmp2))]).reshape(num_areas, num_areas)
+            grad_C_tildZs.append(tmp)
+
+        covAreas = np.hstack(cov_areas_tildZs).reshape(num_areas, num_areas) + np.diag(np.repeat(OMEGA + \
+            np.exp(log_sigma_deltas_of_modelOut), num_areas))
+        return [covAreas, grad_C_tildZs]
+def deri_avg_grad_C_No_obs_noi(x, y, log_sigma, log_w, pointArealFlag = False):
+    n_x = x.shape[0]
+    n_y = y.shape[0]
+
+    C = cov_mat_xy(x, y, np.exp(log_sigma), np.exp(log_w))
+    grad_C = []
+    avg_grad_C = []
+    grad_C_log_sigma = C 
+    grad_C.append(grad_C_log_sigma)
+   
+    tmp1 = np.sum(x * x, axis=1)
+    tmp2 = np.sum(y * y, axis=1)
+    tmp3 = np.dot(x, y.T)
+    
+    tmp_norm = tmp1.reshape(n_x, 1) + tmp2.reshape(1, n_y) - 2 * tmp3
+    grad_C_log_w = grad_C_log_sigma *  (1./np.exp(log_w)**2) * tmp_norm
+    grad_C.append(grad_C_log_w)
+    
+    if pointArealFlag:
+        avg_C = np.mean(C, axis=1)
+        avg_grad_C_log_sigma = np.mean(grad_C_log_sigma, axis =1)
+        avg_grad_C_log_w = np.mean(grad_C_log_w, axis =1)
+    else:
+        avg_C = np.float(np.sum(C))/(n_x * n_y)
+        avg_grad_C_log_sigma = np.float(np.sum(grad_C_log_sigma))/(n_x * n_y)
+        avg_grad_C_log_w = np.float(np.sum(grad_C_log_w))/ (n_x * n_y)
+
+    return [avg_C, avg_grad_C_log_sigma, avg_grad_C_log_w]
+       
+def deri_avg_grad_C_point_and_arealZs_No_obs_noi(x, y, log_sigma, log_w, b, pointArealFlag = False):
+    avg_C, avg_grad_C_log_sigma, avg_grad_C_log_w = deri_avg_grad_C_No_obs_noi(x, y, log_sigma, log_w, pointArealFlag)
+    if pointArealFlag:
+        return [b * avg_C, b * avg_grad_C_log_sigma, b * avg_grad_C_log_w, avg_C]
+    else:
+        return [b**2 * avg_C, b**2 * avg_grad_C_log_sigma, b**2 * avg_grad_C_log_w, 2 * b * avg_C]
 
 def compute_L_chol(cov):
     l_chol = np.linalg.cholesky(cov)
@@ -161,5 +291,140 @@ def log_py_giv_par(theta, X, y, OMEGA = 1e-6):
     log_like  = -np.sum(np.log(np.diag(l_chol_C))) - 0.5 * np.dot(y,u) - 0.5 * n * np.log(2*np.pi)    
 
     return log_like
+
+def predic_gpRegression(theta, X_train, y_train, X_test, y_test, X_tildZs, y_tildZs, gp_deltas_modelOut = True, withPrior= False, \
+    a_bias_poly_deg = 2, rbf = True, OMEGA = 1e-6):
+    theta = np.array(theta)
+    if rbf:
+        num_len_scal = 1
+    else:
+        num_len_scal = X_hatZs.shape(1)
+    if gp_deltas_modelOut:
+        log_sigma_Zs = theta[0] #sigma of GP function for Zs
+        log_phi_Zs = theta[1:num_len_scal+1]  # length scale of GP function for Zs
+        log_obs_noi_scale = theta[num_len_scal+1:num_len_scal+2]
+        log_sigma_deltas_of_modelOut = theta[num_len_scal+2:num_len_scal+3] # sigma of GP function for deltas of model output
+        log_phi_deltas_of_modelOut = theta[num_len_scal+3:num_len_scal+3 + num_len_scal]  # length scale of GP function for for deltas of model output
+        b = theta[num_len_scal+3 + num_len_scal:num_len_scal+3 + num_len_scal+1]
+        a_bias_coefficients = theta[len(theta) - (a_bias_poly_deg+1):]
+        
+    else:
+        log_sigma_Zs = theta[0] #sigma of GP function for Zs
+        log_phi_Zs = theta[1:num_len_scal+1]  # length scale of GP function for Zs
+        log_obs_noi_scale = theta[num_len_scal+1:num_len_scal+2]
+        log_sigma_deltas_of_modelOut = theta[num_len_scal+2:num_len_scal+3] # sigma of Normal for deltas of model output
+        b = theta[num_len_scal+3:num_len_scal+4]
+        a_bias_coefficients = theta[len(theta) - (a_bias_poly_deg+1):]
+
+
+    n_hatZs = X_train.shape[0]
+    n_tildZs = X_tildZs.shape[0]  
+    n_bothZs = n_hatZs + n_tildZs
+
+    mat = np.zeros(n_bothZs * n_bothZs).reshape(n_bothZs, n_bothZs)
+
+    C_hatZs = cov_matrix_reg(X = X_train, sigma = np.exp(log_sigma_Zs), w = np.exp(log_phi_Zs), obs_noi_scale = np.exp(log_obs_noi_scale))
+    if gp_deltas_modelOut:
+        C_tildZs, _= cov_areal(X_tildZs, log_sigma_Zs, log_phi_Zs, b, log_sigma_deltas_of_modelOut, \
+            gp_deltas_modelOut, log_phi_deltas_of_modelOut)
+
+    else:
+        C_tildZs, _ = cov_areal(X_tildZs, log_sigma_Zs, log_phi_Zs, b, log_sigma_deltas_of_modelOut, \
+            gp_deltas_modelOut)
+
+    mat[:n_hatZs, :n_hatZs] = C_hatZs
+    mat[n_hatZs:n_hatZs + n_tildZs, n_hatZs:n_hatZs + n_tildZs] = C_tildZs
+    
+    avg_pointAreal_lower, avg_pointAreal_upper, _, _ = \
+    point_areal(X_train, X_tildZs, log_sigma_Zs, log_phi_Zs, b, pointArealFlag = True)
+
+    mat[n_hatZs:n_hatZs + n_tildZs, :n_hatZs] = avg_pointAreal_lower
+    mat[:n_hatZs, n_hatZs:n_hatZs + n_tildZs] = avg_pointAreal_upper
+
+    mu_train = np.zeros(len(y_train))
+
+    X_tildZs_mean = np.array([np.mean(X_tildZs[i], axis=0) for i in range(len(y_tildZs))])
+    n_row = X_tildZs_mean.shape[0]
+    tmp0 = np.repeat(1.,n_row).reshape(n_row,1)
+    X_tildZs_mean_extend = np.hstack((X_tildZs_mean, tmp0))
+    mu_tildZs = np.dot(X_tildZs_mean_extend, a_bias_coefficients)
+
+    mu_hatTildZs = np.concatenate((mu_train, mu_tildZs))
+
+    y = np.concatenate((y_train, y_tildZs))
+
+    l_chol_C = compute_L_chol(mat)
+    u = linalg.solve_triangular(l_chol_C.T, linalg.solve_triangular(l_chol_C, y - mu_hatTildZs, lower=True))
+    #*******************************comupute the prediction part for ntest test data points under each theta **********************************************************
+    ntest = X_test.shape[0]
+    K_star_star = np.zeros((ntest,1))
+    K_star_hatZs = cov_mat_xy(X_train, X_test, np.exp(log_sigma_Zs), np.exp(log_phi_Zs)) # is a matrix of size (n_train, n_test)
+    K_star_hatZs = K_star_hatZs.T
+    _, avg_pointAreal_upper, _, _ = point_areal(X_test, X_tildZs, log_sigma_Zs, log_phi_Zs, b)
+    K_star_tildZs =avg_pointAreal_upper
+
+    K_star = np.hstack((K_star_hatZs, K_star_tildZs))
+    mu_star = np.dot(K_star, u)
+    print 'estimated mean is ' + str(mu_star)
+    print 'y_test is ' + str(y_test)
+    
+    LKstar = linalg.solve_triangular(l_chol_C, K_star.T, lower = True)
+    for i in range(ntest):
+        K_star_star[i] = cov_matrix(X_test[i].reshape(1, 2), np.exp(log_sigma_Zs), np.exp(log_phi_Zs))
+    
+    vstar = K_star_star - np.sum(LKstar**2, axis=0).reshape(ntest,1) 
+    vstar[vstar < 0] = 1e-9
+    vstar = vstar.reshape(ntest, )
+    print 'estimated variance is ' + str(vstar)
+
+    upper_interv_predic = mu_star + 2 * np.sqrt(vstar + np.exp(log_obs_noi_scale)**2)
+    lower_interv_predic = mu_star - 2 * np.sqrt(vstar + np.exp(log_obs_noi_scale)**2)
+
+    upper_interval_rounded = np.round(upper_interv_predic, 1)
+    lower_interval_rounded = np.round(lower_interv_predic, 1)
+    print 'rounded upper_interval is ' + str(upper_interval_rounded)
+    print 'rounded lower_interval is ' + str(lower_interval_rounded)
+
+    flag_in_confiInterv_r = (y_test >= lower_interval_rounded) & (y_test <= upper_interval_rounded)
+    count_in_confiInterv_r  = np.sum(np.array(map(int, flag_in_confiInterv_r)))
+    print 'number of estimated parameters within the 95 percent confidence interval with rounding is ' + str(count_in_confiInterv_r)
+    succRate = count_in_confiInterv_r/np.float(len(y_test))
+    print 'prediction accuracy is ' + '{:.1%}'.format(succRate)  
+    return succRate
+
+if __name__ == '__main__':
+    computeN3Cost.init(0)
+    p = argparse.ArgumentParser()
+    p.add_argument('-SEED', type=int, dest='SEED', default=99, help='The simulation index')
+    p.add_argument('-numObs', type=int, dest='numObs', default=328, help='Number of observations used in modelling')
+    p.add_argument('-numMo', type=int, dest='numMo', default=300, help='Number of model outputs used in modelling')
+    p.add_argument('-crossValFlag', dest='crossValFlag', default=True,  type=lambda x: (str(x).lower() == 'true'), \
+        help='whether to validate the model using cross validation')
+    args = p.parse_args()
+    input_folder = 'sampRealData/FPstart2016020612_FR' + '_numObs_' + str(args.numObs) + '_numMo_' + str(args.numMo) \
+    + '/seed' + str(args.SEED) + '/fold_9/'
+    
+    X_train_in = open(input_folder + 'X_train.pkl', 'rb')
+    X_train = pickle.load(X_train_in) 
+    y_train_in = open(input_folder + 'y_train.pkl', 'rb')
+    y_train = pickle.load(y_train_in) 
+
+    
+    X_test_in = open(input_folder + 'X_test.pkl', 'rb')
+    X_test = pickle.load(X_test_in)
+    y_test_in = open(input_folder + 'y_test.pkl', 'rb')
+    y_test = pickle.load(y_test_in) 
+
+    X_tildZs_in = open(input_folder + 'X_tildZs.pkl', 'rb')
+    X_tildZs = pickle.load(X_tildZs_in)
+    y_tildZs_in = open(input_folder + 'y_tildZs.pkl', 'rb')
+    y_tildZs = pickle.load(y_tildZs_in)
+
+    #print(X_train.shape, len(y_train), X_test.shape, len(y_test), X_tildZs.shape, len(y_tildZs))
+
+    #theta = np.array( [ 3.72674869 , 0.12894193 , 1.54274378,  1.82996181, -1.96568741,  0.70004865,-0.37216012,  0.51501297, 22.86981066])
+    theta =np.array([ 3.66762091,  0.20002257 ,1.56441102 ,1.84484707, -1.95426797,  0.7243814,-0.40599662,  0.47641285 ,22.67447721])
+    predic_gpRegression(theta, X_train, y_train, X_test, y_test, X_tildZs, y_tildZs)
+
 
 
